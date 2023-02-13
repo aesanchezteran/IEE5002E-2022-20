e0337ab4f6275b7f904a49e63591f3c040d62beb
aesanchezteran <67119559+aesanchezteran@users.noreply.github.com>
2023-02-13 11:00:14 -0500

    Update memfile1.asm

--- ARM Single Cycle MicroArchitecture/memfile1.asm
+++ ARM Single Cycle MicroArchitecture/memfile1.asm
@@ -6,16 +6,16 @@
 
 MAIN      SUB R0, R15, R15 	; R0 = 0				
           ADD R2, R0, #5      	; R2 = 5             
-  		ADD R3, R0, #12    	; R3 = 12            
-  		SUB R7, R3, #9    	; R7 = 3             
-  		ORR R4, R7, R2    	; R4 = 3 OR 5 = 7              	
-  		AND R5, R3, R4    	; R5 = 12 AND 7 = 4            
-  		ADD R5, R5, R4    	; R5 = 4 + 7 = 11              	
-  		SUBS R8, R5, R7    	; R8 <= 11 - 3 = 8, set Flags   	
- 		BEQ END        		; shouldn't be taken            	
-  		SUBS R8, R3, R4    	; R8 = 12 - 7  = 5             
-  		BGE AROUND       	; should be taken               	
-  		ADD R5, R0, #0     	; should be skipped             	
+          ADD R3, R0, #12    	; R3 = 12            
+          SUB R7, R3, #9    	; R7 = 3             
+          ORR R4, R7, R2    	; R4 = 3 OR 5 = 7              	
+          AND R5, R3, R4    	; R5 = 12 AND 7 = 4            
+          ADD R5, R5, R4    	; R5 = 4 + 7 = 11              	
+          SUBS R8, R5, R7    	; R8 <= 11 - 3 = 8, set Flags   	
+          BEQ END        		; shouldn't be taken            	
+          SUBS R8, R3, R4    	; R8 = 12 - 7  = 5             
+          BGE AROUND       	; should be taken               	
+          ADD R5, R0, #0     	; should be skipped             	
  AROUND   SUBS R8, R7, R2   	; R8 = 3 - 5 = -2, set Flags   	
           ADDLT R7, R5, #1  	; R7 = 11 + 1 = 12				
           SUB R7, R7, R2    	; R7 = 12 - 5 = 7				
